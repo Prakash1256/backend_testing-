3d7a6c4aa63b9d1543ce2e1f9283f7321fc88518
prakash <ps4059269@gmail.com>
2025-01-28 22:00:51 +0530

    check on postman

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -23,6 +23,7 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists");
     }
+    console.log(req.user);
 
     const user = await User.create({
         fullName,
